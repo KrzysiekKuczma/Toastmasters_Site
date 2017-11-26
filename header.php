495d7917480b5dfa5620db4e3d664c98077cc425
Krzysiek Kuczma <kuczma.krzysiek@gmail.com>
2017-11-26 21:20:42 +0100

    Działające przygotowanie pod php

--- header.php
+++ header.php
@@ -4,8 +4,6 @@
         <title>Strona Asbirotm</title>
         <meta charset="UTF-8">
         <meta name="viewport" content="width=device-width, initial-scale=1">
-	<link href="./style.css" rel="stylesheet">
-	<script type="text/javascript" src="./js/bundle.js"></script>
 	<?php do_action("wp_head"); ?>
     </head>
     <body <?php body_class(); ?>>
