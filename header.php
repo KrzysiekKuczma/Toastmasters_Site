be8fef5002b4b69c741f46602b15a1f6eb2409a7
Krzysiek Kuczma <kuczma.krzysiek@gmail.com>
2017-12-02 02:25:21 +0100

    zrobiona strona zarządu

--- header.php
+++ header.php
@@ -4,7 +4,6 @@
         <title>Strona Asbirotm</title>
         <meta charset="UTF-8">
         <meta name="viewport" content="width=device-width, initial-scale=1">
-        <link rel="stylesheet" src="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
 	<?php do_action("wp_head"); ?>
     </head>
     <body <?php body_class(); ?>>
